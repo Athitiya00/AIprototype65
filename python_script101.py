48603b33aca5d620130f1ca1106bbdbf80189956
Athitiya00 <athitiya_th@kkumail.com>
2023-02-20 00:41:11 +0700

    Home Work

--- python_script101.py
+++ python_script101.py
@@ -27,58 +27,20 @@ def parse_input():
     args = parser.parse_args()
     return args
 
-def print_other():
-    print('something else')
-
-def print_ones():
-    print('1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 ')
-
-if __name__ == "__main__":
-    x = parse_input() 
-    print_ones()
-    print_other()
-    print_ones()
-
-    print(f'YVAL = {x.yval}')
-    print(f'xt = {(x.x)*2}')
-    # print(f'm = {x.m}')
-    # print(f'n = {x.n}')
-
-# import argparse #สำหรับ รับ input จากภายนอก
-# import subprocess #สำหรับ รัน terminal command
-
-#import flask #สำหรับ ทำ web app และ web service api
-
-
 # def print_other():
 #     print('something else')
 
-# def parse_input():
-#     parser = argparse.ArgumentParser(description='test program to learn about argparse and subprocess')
-#     parser.add_argument(
-#         'm', 
-#         type=int,
-#         help='value of M positional argument')
+# def print_ones():
+#     print('1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 ')
 
-#     parser.add_argument(
-#         '--x', 
-#         type=int,
-#         help='value of x')
+if __name__ == "__main__":
+    x = parse_input() 
+    # print_ones()
+    # print_other()
+    # print_ones()
 
-#     parser.add_argument(
-#         '--yval',
-#         type=int,
-#         default=3,
-#         help='value of y')
+    # print(f'YVAL = {x.yval}')
+    print(f'    xt = {(x.x)*2}')
+    # print(f'm = {x.m}')
+    # print(f'n = {x.n}')
 
-#     args = parser.parse_args()
-#     return args
-
-# if __name__ == "__main__":
-
-    # args = parse_input()
-    
-    # x = args.x
-    # y = args.yval
-    # print(f'M = {args.m}')
-    # print(f'calculate {x} x {y} = {x*y}')
--- python_subprocess.py
+++ python_subprocess.py
@@ -2,26 +2,29 @@ import subprocess #สำหรับ รัน terminal command
 
 if __name__ == "__main__":
     ## basic
-    subprocess.run(["ls", "-ltr"])
-    subprocess.run(["python", "python_script101.py","--x","8"])
+    # subprocess.run(["ls", "-ltr"])
+    # subprocess.run(["python", "python_script101.py","--x","8"])
 
     ## use output of subprocess
-    pro = subprocess.Popen(["python", "python_script101.py","--x","8"],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out, err = pro.communicate()
-    print(out.decode('utf-8').split("\n")[-2])
+    # pro = subprocess.Popen(["python", "python_script101.py","--x","8"],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # out, err = pro.communicate()
+    # print(out.decode('utf-8').split("\n")[-2])
 
 
     ##HW สั่งรัน print python_script101.py 50 รอบ โดย x = 1,3,5,7,....
     #แล้วให้เอา output ของ xt มารวมกันแล้ว print ออกมา 
 
+    sum = 0 
+    round = 0
+    for i in range(1, 100, 2):
+            round +=1
+            process1 = subprocess.Popen(["python", "python_script101.py", "--x",f'{i}'],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            out, err = process1.communicate()
+            print(f"({round}) x = {i}")
+            print(out.decode('utf-8'))
+            sum += (i)
+            
+    print(f'   sum of xt = {(sum)*2}')
 
-    # for i in [2,5,6,8]:
-    #     subprocess.run(["python", "python_script_101.py", "9", "--x", f"{i}", "--yval", "2"])
-    
-
-    # ## use output of subprocess
-    # pro = subprocess.Popen(["ls", "-l"],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    # out, err = pro.communicate()
-    # print(out.decode('utf-8'))
 
 
